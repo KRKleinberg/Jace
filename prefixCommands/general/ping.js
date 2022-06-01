3a9be7294c330949fb6a783dee0bfb256c86a0da
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-01 04:47:29 -0700

    test

--- prefixCommands/general/ping.js
+++ prefixCommands/general/ping.js
@@ -1,7 +1,9 @@
-export const name = "ping";
-export const aliases = [];
-export const description = "Returns websocket ping";
-export const options = [];
+this.name = "ping";
+this.description = "Returns websocket ping";
+//export const name = "ping";
+//export const aliases = [];
+//export const description = "Returns websocket ping";
+//export const options = [];
 export async function run(client, message) {
 	message.channel.send({ content: `📶 | \`${client.ws.ping}ms\`` });
 }
