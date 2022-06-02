6fd07d6a71382f44731fd074aa9ca7e63d53b5ff
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 07:47:19 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -5,34 +5,37 @@ export default {
 	options: [],
 	run: async (client, message) => {
 		const voiceMembers = message.member.voice.channel.members;
-		const teammate = voiceMembers
+		const half = Math.floor(voiceMembers.lenth / 2);
+		const teams = voiceMembers
 			.map((value) => ({ value, sort: Math.random() }))
 			.sort((a, b) => a.sort - b.sort)
 			.map(({ value }) => value);
-		
+		const teamA = teams.slice(0, half);
+		const teamB = teams.slice(half, voiceMembers.lenth);
+
 		message.channel.send({
-	embeds: [
-		{
-			title: "Teams",
-			fields: [
+			embeds: [
 				{
-					name: "Team A",
-					value: `${teammate}`,
-					inline: true,
-				},
-				{
-					name: "Team B",
-					value: "Kevin\nRyan",
-					inline: true,
-				},
-				{
-					name: "Map Choice",
-					value: "Kevin",
+					title: "Teams",
+					fields: [
+						{
+							name: "Team A",
+							value: `${teamA.join("\n")}`,
+							inline: true,
+						},
+						{
+							name: "Team B",
+							value: `${teamB.join("\n")}`,
+							inline: true,
+						},
+						{
+							name: "Map Choice",
+							value: `${teams[0]}`,
+						},
+					],
+					color: 0x5864f1,
 				},
 			],
-			color: 0x5864f1,
-		},
-	],
-});
+		});
 	},
 };
