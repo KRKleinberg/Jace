6d54992ebd2beb593c0635541254971f306ecfef
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 08:26:19 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -6,41 +6,46 @@ export default {
 	run: async (client, message) => {
 		const voiceMembers = message.member.voice.channel.members;
 		const half = Math.ceil(voiceMembers.lenth / 2);
-		const teams = voiceMembers
-			.map((value) => ({ value, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ value }) => value);
-		const teamA = `${teams.slice(0, half).join("\n")}`;
-		const teamB = `${teams.slice(half).join("\n")}`;
-		const mapChoice = `${teams[Math.ceil(Math.random() * teams.length)]}`;
+		function shuffle(array) {
+			// eslint-disable-next-line no-plusplus
+			for (let i = array.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				// eslint-disable-next-line no-param-reassign
+				[array[i], array[j]] = [array[j], array[i]];
+			}
+		}
+		const shuffled = shuffle(voiceMembers);
+		const teamA = shuffled.slice(0, half).join("\n");
+		const teamB = shuffled.slice(half).join("\n");
+		const mapChoice = shuffled[Math.ceil(Math.random() * shuffled.length)];
 
 		message.channel.send({
-	embeds: [
-		{
-			title: "Teams",
-			fields: [
+			embeds: [
 				{
-					name: "Team A",
-					value: `1 ${teamA}`,
-					inline: true,
-				},
-				{
-					name: "Team B",
-					value: `2 ${teamB}`,
-					inline: true,
-				},
-				{
-					name: "Map Choice",
-					value: `3 ${mapChoice}`,
-				},
-				{
-					name: "Map Choice",
-					value: `3 ${teams}`,
+					title: "Teams",
+					fields: [
+						{
+							name: "Team A",
+							value: `1 ${teamA}`,
+							inline: true,
+						},
+						{
+							name: "Team B",
+							value: `2 ${teamB}`,
+							inline: true,
+						},
+						{
+							name: "Map Choice",
+							value: `3 ${mapChoice}`,
+						},
+						{
+							name: "Map Choice",
+							value: `3 ${shuffled} ${shuffled.length}`,
+						},
+					],
+					color: 0x5864f1,
 				},
 			],
-			color: 0x5864f1,
-		},
-	],
-});
+		});
 	},
 };
