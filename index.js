02ed70fd474f3a9345beaf01a177f87f17d8ec68
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 05:59:03 -0700

    fix

--- index.js
+++ index.js
@@ -4,31 +4,33 @@ import handler from "./handler.js";
 
 const client = new Client({
 	intents: 32767,
-	playerOptions: {
-		ytdlOptions: {
-			requestOptions: {
-				headers: {
-					cookie: process.env.COOKIE,
-					"x-youtube-identity-token": process.env.ID_TOKEN,
-				},
-			},
-			quality: "highest",
-			filter: "audioonly",
-			// eslint-disable-next-line no-bitwise
-			highWaterMark: 1 << 25,
-			dlChunkSize: 0,
-		},
-		leaveOnEnd: false,
-		leaveOnStop: true,
-		leaveOnEmpty: false,
-		leaveOnEmptyCooldown: 5000,
-		autoSelfDeaf: true,
-	},
-	prefixCommands: new Collection(),
-	slashCommands: new Collection(),
 });
 
+client.playerOptions = {
+	ytdlOptions: {
+		requestOptions: {
+			headers: {
+				cookie: process.env.COOKIE,
+				"x-youtube-identity-token": process.env.ID_TOKEN,
+			},
+		},
+		quality: "highest",
+		filter: "audioonly",
+		// eslint-disable-next-line no-bitwise
+		highWaterMark: 1 << 25,
+		dlChunkSize: 0,
+	},
+	leaveOnEnd: false,
+	leaveOnStop: true,
+	leaveOnEmpty: false,
+	leaveOnEmptyCooldown: 5000,
+	autoSelfDeaf: true,
+};
+
 client.player = new Player(client, client.playerOptions);
+
+client.prefixCommands = new Collection();
+client.slashCommands = new Collection();
 
 export default client;
 
