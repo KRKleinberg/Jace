a28ad82598fbe0db097df86dbdb0f853d915f030
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 03:18:28 -0700

    test

---
+++ prefixCommands/music/queue.js
@@ -0,0 +1,34 @@
+export default {
+	name: "queue",
+	aliases: ["q"],
+	description: "Displays the queue",
+	options: [],
+	run: async (client, message) => {
+		const queue = client.player.getQueue(message.guildId);
+		if (!queue || !queue.playing)
+			return message.channel.send({ content: "❌ | No music is being played!" });
+		const currentTrack = queue.current;
+		const tracks = queue.tracks.slice(0, 10).map((m, i) => `${i + 1}. **${m.title}** ([link](${m.url}))`);
+
+		return message.channel.send({
+			embeds: [
+				{
+					title: "Server Queue",
+					description: `${tracks.join("\n")}${
+						queue.tracks.length > tracks.length
+							? `\n...${
+									queue.tracks.length - tracks.length === 1
+										? `${queue.tracks.length - tracks.length} more track`
+										: `${queue.tracks.length - tracks.length} more tracks`
+							  }`
+							: ""
+					}`,
+					color: 0x5864f1,
+					fields: [
+						{ name: "Now Playing", value: `🎶 | **${currentTrack.title}** ([link](${currentTrack.url}))` },
+					],
+				},
+			],
+		});
+	},
+};
