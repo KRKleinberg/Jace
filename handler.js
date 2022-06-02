78fb88c62965954559d3a8fea048eedc9ba5b36b
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-01 23:19:38 -0700

    test

--- handler.js
+++ handler.js
@@ -1,7 +1,1 @@
-import prefixCommands from "./prefixCommands/**/*.js";
-import slashCommands from "./slashCommands/**/*.js";
-import globPKG from "glob";
-const { glob } = globPKG;
 
-
-console.log(prefixCommands);
