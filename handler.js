3762e84530fb70e016066c6836d9349b9f0c845e
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-01 20:27:14 -0700

    test new handler

--- handler.js
+++ handler.js
@@ -1,44 +1,4 @@
-import globPKG from "glob";
-import { promisify } from "util";
-const { glob } = globPKG;
-const globPromise = promisify(glob);
+import prefixCommands from "./prefixCommands/**/*.js";
+import slashCommands from "./slashCommands/**/*.js";
 
-export default async (client) => {
-	const prefixCommandFiles = await globPromise("./prefixCommands/**/*.js");
-	prefixCommandFiles.map((value) => {
-		const file = import(value);
-		const splitted = value.split("/");
-		const directory = splitted[splitted.length - 2];
-
-		if (file.name) {
-			const properties = { directory, ...file };
-			client.prefixCommands.set(file.name, properties);
-		}
-	});
-
-	prefixCommandFiles.then(function (file) {
-		console.log(file);
-	});
-
-	const eventFiles = await globPromise("./events/**/*.js");
-	eventFiles.map((value) => import(value));
-
-	const slashCommands = await globPromise("./slashCommands/**/*.js");
-
-	const arrayOfSlashCommands = [];
-	slashCommands.map((value) => {
-		const file = import(value);
-		if (!file?.name) return;
-		client.slashCommands.set(file.name, file);
-
-		if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-		arrayOfSlashCommands.push(file);
-	});
-
-	client.on("ready", async () => {
-		await client.application.commands.set(arrayOfSlashCommands);
-	});
-
-	// Comment out the line below for main branch
-	setTimeout(function () { process.exit(0); }, 300000);
-};
+console.log(prefixCommands);
---
+++ handler0.js
@@ -0,0 +1,40 @@
+import globPKG from "glob";
+import { promisify } from "util";
+const { glob } = globPKG;
+const globPromise = promisify(glob);
+
+export default async (client) => {
+	const prefixCommandFiles = await globPromise("./prefixCommands/**/*.js");
+	prefixCommandFiles.map((value) => {
+		const file = import(value);
+		const splitted = value.split("/");
+		const directory = splitted[splitted.length - 2];
+
+		if (file.name) {
+			const properties = { directory, ...file };
+			client.prefixCommands.set(file.name, properties);
+		}
+	});
+
+	const eventFiles = await globPromise("./events/**/*.js");
+	eventFiles.map((value) => import(value));
+
+	const slashCommands = await globPromise("./slashCommands/**/*.js");
+
+	const arrayOfSlashCommands = [];
+	slashCommands.map((value) => {
+		const file = import(value);
+		if (!file?.name) return;
+		client.slashCommands.set(file.name, file);
+
+		if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+		arrayOfSlashCommands.push(file);
+	});
+
+	client.on("ready", async () => {
+		await client.application.commands.set(arrayOfSlashCommands);
+	});
+
+	// Comment out the line below for main branch
+	setTimeout(function () { process.exit(0); }, 300000);
+};
