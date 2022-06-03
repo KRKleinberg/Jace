9a896d00ac5335817d6ab1c2d884f6efaf0c0d2e
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 23:24:16 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -4,39 +4,41 @@ export default {
 	description: "Splits users in voice channel into two teams",
 	options: [],
 	run: async (client, message) => {
-		const voiceMembers = message.member.voice.channel.members.filter((member) => !member.user.bot);
-		const shuffled = voiceMembers
-			.map((value) => ({ value, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ value }) => value);
-		const half = Math.ceil(shuffled.length / 2);
-		const teamA = shuffled.slice(0, half);
-		const teamB = shuffled.slice(half);
-		const mapChoice = shuffled[Math.floor(Math.random() * shuffled.length)];
+		if (message.member.voice.channel) {
+			const voiceMembers = message.member.voice.channel.members.filter((member) => !member.user.bot);
+			const shuffled = voiceMembers
+				.map((value) => ({ value, sort: Math.random() }))
+				.sort((a, b) => a.sort - b.sort)
+				.map(({ value }) => value);
+			const half = Math.ceil(shuffled.length / 2);
+			const teamA = shuffled.slice(0, half);
+			const teamB = shuffled.slice(half);
+			const mapChoice = shuffled[Math.floor(Math.random() * shuffled.length)];
 
-		message.channel.send({
-			embeds: [
-				{
-					title: "Teams",
-					fields: [
-						{
-							name: "Team A",
-							value: `${teamA.join("\n")}\u200B`,
-							inline: true,
-						},
-						{
-							name: "Team B",
-							value: `${teamB.join("\n").toString()}\u200B`,
-							inline: true,
-						},
-						{
-							name: "Map Choice",
-							value: `${mapChoice.toString()}\u200B`,
-						},
-					],
-					color: 0x5864f1,
-				},
-			],
-		});
+			message.channel.send({
+				embeds: [
+					{
+						title: "Teams",
+						fields: [
+							{
+								name: "Team A",
+								value: `${teamA.join("\n")}\u200B`,
+								inline: true,
+							},
+							{
+								name: "Team B",
+								value: `${teamB.join("\n").toString()}\u200B`,
+								inline: true,
+							},
+							{
+								name: "Map Choice",
+								value: `${mapChoice.toString()}\u200B`,
+							},
+						],
+						color: 0x5864f1,
+					},
+				],
+			});
+		} else message.channel.send({ content: "You're not in a voice channel!" });
 	},
 };
--- slashCommands/general/teams.js
+++ slashCommands/general/teams.js
@@ -2,39 +2,41 @@ export default {
 	name: "teams",
 	description: "Splits users in voice channel into two teams",
 	run: async (client, interaction) => {
-		const voiceMembers = interaction.member.voice.channel.members.filter((member) => !member.user.bot);
-		const shuffled = voiceMembers
-			.map((value) => ({ value, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ value }) => value);
-		const half = Math.ceil(shuffled.length / 2);
-		const teamA = shuffled.slice(0, half);
-		const teamB = shuffled.slice(half);
-		const mapChoice = shuffled[Math.floor(Math.random() * shuffled.length)];
+		if (interaction.member.voice.channel) {
+			const voiceMembers = interaction.member.voice.channel.members.filter((member) => !member.user.bot);
+			const shuffled = voiceMembers
+				.map((value) => ({ value, sort: Math.random() }))
+				.sort((a, b) => a.sort - b.sort)
+				.map(({ value }) => value);
+			const half = Math.ceil(shuffled.length / 2);
+			const teamA = shuffled.slice(0, half);
+			const teamB = shuffled.slice(half);
+			const mapChoice = shuffled[Math.floor(Math.random() * shuffled.length)];
 
-		interaction.followUp({
-			embeds: [
-				{
-					title: "Teams",
-					fields: [
-						{
-							name: "Team A",
-							value: `${teamA.join("\n")}\u200B`,
-							inline: true,
-						},
-						{
-							name: "Team B",
-							value: `${teamB.join("\n").toString()}\u200B`,
-							inline: true,
-						},
-						{
-							name: "Map Choice",
-							value: `${mapChoice.toString()}\u200B`,
-						},
-					],
-					color: 0x5864f1,
-				},
-			],
-		});
+			interaction.followUp({
+				embeds: [
+					{
+						title: "Teams",
+						fields: [
+							{
+								name: "Team A",
+								value: `${teamA.join("\n")}\u200B`,
+								inline: true,
+							},
+							{
+								name: "Team B",
+								value: `${teamB.join("\n").toString()}\u200B`,
+								inline: true,
+							},
+							{
+								name: "Map Choice",
+								value: `${mapChoice.toString()}\u200B`,
+							},
+						],
+						color: 0x5864f1,
+					},
+				],
+			});
+		} else interaction.followUp({ content: "Nobody's in a voice channel!" });
 	},
 };
