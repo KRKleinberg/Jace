8ba9247d265b36edfd0d3b0cf83bc8c86a26a4c2
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-03 06:08:46 -0700

    test

---
+++ prefixCommands/music/resume.js
@@ -0,0 +1,17 @@
+export default {
+	name: "resume",
+	aliases: [],
+	description: "Resumes the current song",
+	options: [],
+	run: async (client, message) => {
+		if (message.member.voice.channel) {
+			const queue = client.player.getQueue(message.guildId);
+
+			if (queue && queue.playing) {
+				const resumed = queue.setPaused(false);
+
+				message.channel.send({ content: resumed ? "▶ | Resumed!" : "❌ | Something went wrong!" });
+			} else message.channel.send({ content: "❌ | No music is being played!" });
+		} else message.channel.send({ content: "❌ | You're not in a voice channel!" });
+	},
+};
---
+++ slashCommands/music/resume.js
@@ -0,0 +1,15 @@
+export default {
+	name: "resume",
+	description: "Resumes the current song",
+	run: async (client, interaction) => {
+		if (interaction.member.voice.channel) {
+			const queue = client.player.getQueue(interaction.guildId);
+
+			if (queue && queue.playing) {
+				const resumed = queue.setPaused(false);
+
+				interaction.deferReply({ content: resumed ? "▶ | Resumed!" : "❌ | Something went wrong!" });
+			} else interaction.deferReply({ content: "❌ | No music is being played!" });
+		} else interaction.deferReply({ content: "❌ | You're not in a voice channel!" });
+	},
+};
