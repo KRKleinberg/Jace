612a9f9629c07adc8c9b8136bed64eb606df6f68
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-03 09:04:32 -0700

    test

--- slashCommands/general/restart.js
+++ slashCommands/general/restart.js
@@ -2,13 +2,13 @@ export default {
 	name: "restart",
 	description: "Restarts the bot",
 	run: async (client, interaction) => {
-		await interaction.followUp({ content: `🔄️ | <@${client.user.id}> is restarting` });
-		await client.user.setStatus("idle");
-		await client.user.setActivity({
+		interaction.followUp({ content: `🔄️ | <@${client.user.id}> is restarting` });
+		client.user.setStatus("idle");
+		client.user.setActivity({
 			name: "Restarting...",
 			type: "PLAYING",
 		});
-        
+
 		console.log("Bot is restarting...");
 
 		process.exit(1);
