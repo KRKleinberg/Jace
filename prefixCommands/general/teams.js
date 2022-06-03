d6ae3afda035572e51265f3d6a1df43e94751949
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 19:23:54 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -35,7 +35,7 @@ export default {
 						},
 						{
 							name: "Map Choice",
-							value: `3 ${shuffled} ${voiceMembers.length/2}`,
+							value: `3 ${shuffled} ${voiceMembers.length}`,
 						},
 					],
 					color: 0x5864f1,
