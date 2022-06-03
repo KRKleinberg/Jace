fc019fa89b6120525ca432b548773a2ec4b7c6fb
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 23:47:31 -0700

    test

--- slashCommands/general/teams.js
+++ slashCommands/general/teams.js
@@ -37,6 +37,6 @@ export default {
 					},
 				],
 			});
-		} else interaction.followUp({ content: "Nobody's in a voice channel!" });
+		} else interaction.followUp({ content: "You're not in a voice channel!" });
 	},
 };
