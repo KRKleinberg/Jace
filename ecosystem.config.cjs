0bbb6e1d90a5a50872183e18a0919c750ca7bb3a
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-03 07:16:14 -0700

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
 			name: "jace-bot",
 			script: "./index.js",
 			instances: "max",
-			exec_mode: "cluster",
 			stop_exit_codes: [0],
 			exp_backoff_restart_delay: 100,
 			env_production: {
