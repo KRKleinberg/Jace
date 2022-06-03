47899737739da94ef7e43f7cb463361996daa7e0
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-03 00:31:11 -0700

    test

--- prefixCommands/music/remove.js
+++ prefixCommands/music/remove.js
@@ -6,12 +6,13 @@ export default {
 	run: async (client, message, args) => {
 		const queue = client.player.getQueue(message.guildId);
 		if (queue || queue.playing) {
-			const trackIndex = parseInt(args[0], 10) - 1;
+            const trackIndex = parseInt(args[0], 10) - 1;
+            const trackName = queue.tracks[trackIndex];
 			const success = queue.remove(trackIndex);
 
 			message.channel.send({
 				content: success
-					? `➖ | Removed track ${queue.tracks[trackIndex-1]}.`
+					? `➖ | Removed track ${trackName}.`
 					: "❌ | Please enter a valid track number in the queue",
 			});
 		} else message.channel.send({ content: "❌ | No music is being played!" });
--- slashCommands/music/remove.js
+++ slashCommands/music/remove.js
@@ -1,21 +1,26 @@
 export default {
-    name: 'remove',
-    description: 'Removes a specific track',
-    options: [
-        {
-            name: 'track',
-            description: 'The number of the track in the queue to remove',
-            type: 'INTEGER',
-            required: true
-        }
-    ],
-    run: async (client, interaction) => {
-        const queue = client.player.getQueue(interaction.guildId);
-        if (queue || queue.playing) {
-            const trackIndex = interaction.options.getNumber("track") - 1;
-            const success = queue.remove(trackIndex);
+	name: "remove",
+	description: "Removes a specific track",
+	options: [
+		{
+			name: "track",
+			description: "The number of the track in the queue to remove",
+			type: "INTEGER",
+			required: true,
+		},
+	],
+	run: async (client, interaction) => {
+		const queue = client.player.getQueue(interaction.guildId);
+		if (queue || queue.playing) {
+			const trackIndex = interaction.options.getNumber("track") - 1;
+			const trackName = queue.tracks[trackIndex];
+			const success = queue.remove(trackIndex);
 
-            interaction.followUp({ content: success ? `➖ | Removed track ${queue.tracks[trackIndex-1]}.` : '❌ | Please enter a valid track number in the queue' });
-        } else interaction.followUp({ content: "❌ | No music is being played!" });
-    },
+			interaction.followUp({
+				content: success
+					? `➖ | Removed *${trackName}*.`
+					: "❌ | Please enter a valid track number in the queue",
+			});
+		} else interaction.followUp({ content: "❌ | No music is being played!" });
+	},
 };
