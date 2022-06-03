91bcbf109a4fe33e1885801148accd8573ccde8a
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 19:26:21 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -4,7 +4,7 @@ export default {
 	description: "Splits users in voice channel into two teams",
 	options: [],
 	run: async (client, message) => {
-		const voiceMembers = message.member.voice.channel.members;
+		const voiceMembers = Array.from(message.member.voice.channel.members);
 		const half = Math.ceil(voiceMembers.length / 2);
 		const shuffled = voiceMembers
 			.map((value) => ({ value, sort: Math.random() }))
