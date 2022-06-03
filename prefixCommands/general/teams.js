bdfe983f86b5c5975519f079e23eab7adea0c260
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 18:29:10 -0700

    test

--- prefixCommands/general/teams.js
+++ prefixCommands/general/teams.js
@@ -6,48 +6,41 @@ export default {
 	run: async (client, message) => {
 		const voiceMembers = message.member.voice.channel.members;
 		const half = Math.floor(voiceMembers.lenth / 2);
-		function shuffle(array) {
-			const shuffled = array;
-			// eslint-disable-next-line no-plusplus
-			for (let i = shuffled.length - 1; i > 0; i--) {
-				const j = Math.floor(Math.random() * (i + 1));
-				// eslint-disable-next-line no-param-reassign
-				[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-			}
-			return shuffled;
-		}
-		const shuffled = shuffle(voiceMembers);
+		const shuffled = voiceMembers
+			.map((value) => ({ value, sort: Math.random() }))
+			.sort((a, b) => a.sort - b.sort)
+			.map(({ value }) => value);
 		const teamA = shuffled.slice(0, half);
 		const teamB = shuffled.slice(-half);
 		const mapChoice = shuffled[Math.floor(Math.random() * shuffled.length)];
 
 		message.channel.send({
-	embeds: [
-		{
-			title: "Teams",
-			fields: [
+			embeds: [
 				{
-					name: "Team A",
-					value: `1 ${teamA}`,
-					inline: true,
-				},
-				{
-					name: "Team B",
-					value: `2 ${teamB}`,
-					inline: true,
-				},
-				{
-					name: "Map Choice",
-					value: `3 ${mapChoice}`,
-				},
-				{
-					name: "Map Choice",
-					value: `3 ${shuffled} ${shuffled.length}`,
+					title: "Teams",
+					fields: [
+						{
+							name: "Team A",
+							value: `1 ${teamA}`,
+							inline: true,
+						},
+						{
+							name: "Team B",
+							value: `2 ${teamB}`,
+							inline: true,
+						},
+						{
+							name: "Map Choice",
+							value: `3 ${mapChoice}`,
+						},
+						{
+							name: "Map Choice",
+							value: `3 ${shuffled} ${shuffled.length}`,
+						},
+					],
+					color: 0x5864f1,
 				},
 			],
-			color: 0x5864f1,
-		},
-	],
-});
+		});
 	},
 };
