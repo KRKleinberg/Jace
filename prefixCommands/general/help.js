cb0e91cff02cbe9b445162dc966d9664ccb481c8
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 23:52:39 -0700

    test

--- prefixCommands/general/help.js
+++ prefixCommands/general/help.js
@@ -6,14 +6,13 @@ export default {
 	run: async (client, message) => {
 		// Iterate over the commands and create field objects
 		// If there are less than 25 fields, you can safely send the embed in a single message
-		const fields = client.commands
-			.map((command) => ({
-				name: command.aliases.length
-					? `${command.name} (${command.aliases.join(", ")})`
-					: `${command.name}`,
-				value: `${command.description}`,
-			}))
-			.slice(0, 25);
+		const fields = client.commands.map((command) => ({
+			name: command.aliases.length
+				? `${command.name} (${command.aliases.join(", ")})`
+				: `${command.name}`,
+			value: `${command.description}`,
+		}));
+		// .slice(0, 25);
 
 		message.channel.send({
 			embeds: [
--- slashCommands/general/help.js
+++ slashCommands/general/help.js
@@ -4,14 +4,13 @@ export default {
 	run: async (client, interaction) => {
 		// Iterate over the commands and create field objects
 		// If there are less than 25 fields, you can safely send the embed in a single message
-		const fields = client.commands
-			.map((command) => ({
-				name: command.aliases.length
-					? `${command.name} (${command.aliases.join(", ")})`
-					: `${command.name}`,
-				value: `${command.description}`,
-			}))
-			.slice(0, 25);
+		const fields = client.commands.map((command) => ({
+			name: command.aliases.length
+				? `${command.name} (${command.aliases.join(", ")})`
+				: `${command.name}`,
+			value: `${command.description}`,
+		}));
+		// .slice(0, 25);
 
 		interaction.followUp({
 			embeds: [
