d8aa515307e2ffb120bc28d3c4b1e32f41aa2946
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-02 23:55:45 -0700

    test

--- slashCommands/general/help.js
+++ slashCommands/general/help.js
@@ -2,25 +2,27 @@ export default {
 	name: "help",
 	description: `Displays a list of all commands`,
 	run: async (client, interaction) => {
+		const MAX_FIELDS = 25;
 		// Iterate over the commands and create field objects
-		// If there are less than 25 fields, you can safely send the embed in a single message
-		const fields = client.commands.map((command) => ({
+		const fields = client.prefixCommands.map((command) => ({
 			name: command.aliases.length
 				? `${command.name} (${command.aliases.join(", ")})`
 				: `${command.name}`,
 			value: `${command.description}`,
-		}));
-		// .slice(0, 25);
+		})).slice();
 
-		interaction.followUp({
-			embeds: [
-				{
-					title: "Commands",
-					description: `Prefix: **${process.env.PREFIX}**`,
-					fields,
-					color: 0x5864f1,
-				},
-			],
-		});
+		// If there are less than 25 fields, you can safely send the embed in a single message
+		if (fields.length <= MAX_FIELDS)
+			interaction.followUp({
+				embeds: [
+					{
+						title: "Commands",
+						description: `Prefix: **${process.env.PREFIX}**`,
+						fields,
+						color: 0x5864f1,
+					},
+				],
+			});
+
 	},
 };
