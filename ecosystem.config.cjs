c5cd35b103259889691063c774b9a43f25f24dd7
K2Kleinberg <k2kleinberg@gmail.com>
2022-06-03 07:08:40 -0700

    Test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,12 @@ module.exports = {
 			exec_mode: "cluster",
 			stop_exit_codes: [0],
 			exp_backoff_restart_delay: 100,
+			env_production: {
+				NODE_ENV: "production",
+			},
+			env_development: {
+				NODE_ENV: "development",
+			},
 		},
 	],
 };
