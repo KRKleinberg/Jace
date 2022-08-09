6be3cc097c661cf2173f88f087c40ed7eba11b19
K2Kleinberg <k2kleinberg@gmail.com>
2022-08-09 07:16:07 -0700

    test

--- src/handler.ts
+++ src/handler.ts
@@ -3,7 +3,7 @@ import { prefixCommands, slashCommands } from "./index.js";
 
 export default async () => {
 	// Event handler
-	const eventFiles: string[] = await globby("./**", { cwd: "./" });
+	const eventFiles: string[] = await globby("**", { cwd: "./" });
 
 	console.log(eventFiles);
 
