4943bbbfaa56430889ba93ccce124fc1c3f6a27a
K2Kleinberg <k2kleinberg@gmail.com>
2022-08-09 19:17:31 -0700

    test

--- src/events/commands.ts
+++ src/events/commands.ts
@@ -1,4 +1,5 @@
-import { client, prefixCommands } from "../index.js";
+import { CommandInteractionOption, CacheType } from "discord.js";
+import { client, prefixCommands, slashCommands } from "../index.js";
 
 client.on("messageCreate", async (message) => {
 	if (
@@ -13,5 +14,31 @@ client.on("messageCreate", async (message) => {
 			prefixCommands.find((c) => c.aliases?.includes(input.toLowerCase()));
 
 		if (prefixCommand) await prefixCommand.run(client, message, args);
+	}
+});
+
+client.on("interactionCreate", async (interaction) => {
+	// Slash Command Handling
+	if (interaction.isChatInputCommand()) {
+		await interaction.followUp({ ephemeral: false }).catch(() => {});
+
+		const slashCommand = slashCommands.get(interaction.commandName);
+
+		if (slashCommand) {
+			const args: any[] = [];
+			const options: readonly CommandInteractionOption<CacheType>[] = interaction.options.data;
+
+			options.forEach((option: any) => {
+				if (option.type === "SUB_COMMAND") {
+					if (option.name) args.push(option.name);
+
+					option.options.forEach((x: any) => {
+						if (x.value) args.push(x.value);
+					});
+				} else if (option.value) args.push(option.value);
+			});
+
+			slashCommand.run(client, interaction, args);
+		} else interaction.followUp({ content: "An error has occured" });
 	}
 });
