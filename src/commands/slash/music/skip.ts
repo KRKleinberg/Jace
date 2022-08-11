7bf7834d8ffd644aa09087cfea7c49cc17f64c6c
K2Kleinberg <k2kleinberg@gmail.com>
2022-08-11 06:54:08 -0700

    test

---
+++ src/commands/slash/music/skip.ts
@@ -0,0 +1,20 @@
+import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js';
+import { player } from '../../../index.js';
+
+export default {
+	data: new SlashCommandBuilder().setName('skip').setDescription('Skips the current song'),
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		const queue = player.getQueue(interaction.guild!);
+		const currentTrack = queue.current;
+
+		if (!(interaction.member as GuildMember).voice.channel)
+			return await interaction.reply({ content: '❌ | You are not in a voice channel!' });
+		if (!queue || !queue.playing)
+			return await interaction.reply({ content: '❌ | No music is playing!' });
+
+		return await interaction.reply({
+			content: queue.skip() ? `⏭️ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
+		});
+	},
+};
