808b8bfbe0b5ff691582e730bb0f4b6ff09cc77a
Kevin Kleinberg <73312027+KRKleinberg@users.noreply.github.com>
2022-08-13 07:11:08 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import play from 'play-dl';
 // Check environment variables
 if (!process.env.APP_ID) throw new Error('APP_ID environment variable is not set!');
 if (!process.env.COOKIE) throw new Error('COOKIE environment variable is not set!');
-if (!process.env.DISCORD_TOKEN) throw new Error('DJS_TOKEN environment variable is not set!');
+if (!process.env.DJS_TOKEN) throw new Error('DJS_TOKEN environment variable is not set!');
 if (!process.env.HEROKU_BRANCH) throw new Error('HEROKU_BRANCH environment variable is not set!');
 if (!process.env.ID_TOKEN) throw new Error('ID_TOKEN environment variable is not set!');
 if (!process.env.PREFIX) throw new Error('PREFIX environment variable is not set!');
@@ -116,3 +116,4 @@ if (process.env.HEROKU_BRANCH === 'dev') {
 
 // Start
 client.login(process.env.DJS_TOKEN);
+
