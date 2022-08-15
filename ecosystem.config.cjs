fe26bc9fa98439bbc8fd9866ba92829920077b94
K2Kleinberg <k2kleinberg@gmail.com>
2022-08-15 05:01:19 -0700

    Added ignore files for ESLint and Prettier

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	apps: [
 		{
-			name: "jace-bot",
-			script: "node dist/index.js",
+			name: 'jace-bot',
+			script: 'node dist/index.js',
 			exp_backoff_restart_delay: 100,
 		},
 	],
