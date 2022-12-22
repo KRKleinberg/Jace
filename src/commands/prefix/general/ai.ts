77a0fa1cea391dc5c4d7777cf9742c7df6821f02
Kevin Kleinberg <krkleinberg@gmail.com>
2022-12-21 22:12:18 -0600

    Updated ai commands

--- src/commands/prefix/general/ai.ts
+++ src/commands/prefix/general/ai.ts
@@ -17,7 +17,7 @@ export default {
 				model: 'text-davinci-003',
 				prompt,
 				temperature: 0.7,
-				max_tokens: 2048,
+				max_tokens: 4000,
 				top_p: 1,
 				frequency_penalty: 0.5,
 				presence_penalty: 0.5,
