ae70477dac100e6e7ddd58236aed0761a5f25ca1
Kevin Kleinberg <dev@krkleinberg.me>
2025-02-19 23:33:48 +0100

    Added additional temporary debugging helpers to ping command

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,6 +1,6 @@
 import { App } from '#utils/app';
 import { Player } from '#utils/player';
-import { useQueue } from 'discord-player';
+import { type Track, useQueue } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 export const command: App.Command = {
@@ -9,10 +9,55 @@ export const command: App.Command = {
 		// REMOVE LATER
 		{
 			const queue = useQueue();
+			const currentTrack = queue?.currentTrack;
+			const tracks: Track[] =
+				currentTrack && queue.tracks.toArray().length ? [currentTrack, ...queue.tracks.toArray()] : [];
 
 			queue?.delete();
 
 			await Player.initializeExtractors();
+
+			queue?.revive();
+
+			await queue?.tasksQueue.acquire().getTask();
+
+			if (!queue?.connection) {
+				try {
+					if (ctx.member.voice.channel) {
+						await queue?.connect(ctx.member.voice.channel);
+					}
+				} catch (error) {
+					console.error(error);
+
+					queue?.tasksQueue.release();
+
+					return await App.respond(ctx, 'Could not join your voice channel', App.ResponseType.AppError);
+				}
+			}
+
+			try {
+				if (tracks.length) {
+					queue?.addTrack(tracks);
+				}
+			} catch (error) {
+				console.error(error);
+
+				queue?.tasksQueue.release();
+
+				return await App.respond(ctx, 'Could not add that track', App.ResponseType.AppError);
+			}
+
+			try {
+				if (!queue?.isPlaying()) {
+					await queue?.node.play();
+				}
+			} catch (error) {
+				console.error(error);
+
+				return await App.respond(ctx, 'Could not play this track', App.ResponseType.AppError);
+			} finally {
+				queue?.tasksQueue.release();
+			}
 		}
 
 		return await App.respond(ctx, `📶\u2002${ctx.command.client.ws.ping.toString()} ms`);
