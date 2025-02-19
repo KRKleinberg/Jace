9f36930eca0f3e35f0dde0dbe686747ab8b76f34
Kevin Kleinberg <dev@krkleinberg.me>
2025-02-19 18:30:50 +0100

    Added temporary player debug helper to ping command

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,9 +1,20 @@
 import { App } from '#utils/app';
+import { Player } from '#utils/player';
+import { useQueue } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 export const command: App.Command = {
 	data: new SlashCommandBuilder().setDescription('Displays app network latency'),
 	async run(ctx) {
+		// REMOVE LATER
+		{
+			const queue = useQueue();
+
+			queue?.delete();
+
+			await Player.initializeExtractors();
+		}
+
 		return await App.respond(ctx, `📶\u2002${ctx.command.client.ws.ping.toString()} ms`);
 	},
 };
