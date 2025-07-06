bb2643bbedf8165f4b81085d5d534f4315c5f5f7
Kevin Kleinberg <dev@krkleinberg.me>
2025-07-06 02:10:09 +0200

    Revert "Add temp debug logs"
    This reverts commit 7c0817e448dd5132d0fc2e2bff7c8b7b90c7d6b8.

--- src/utils/player/extractors/deezer.ts
+++ src/utils/player/extractors/deezer.ts
@@ -88,7 +88,6 @@ export class DeezerExtractor extends DZExtractor {
 			throw new Error(`No Deezer track found for "${track.title}" by ${track.author}`);
 		}
 
-		console.log('ATTEMPT STREAM', deezerTrack.title, deezerTrack.author);
 		const stream = await this.stream(deezerTrack);
 
 		track.bridgedExtractor = this;
@@ -98,8 +97,6 @@ export class DeezerExtractor extends DZExtractor {
 			track.duration = deezerTrack.duration;
 		}
 
-		console.log('Bridged track:', deezerTrack.title, deezerTrack.author);
-		console.log('Bridged stream:', stream);
 		return stream;
 	}
 }
